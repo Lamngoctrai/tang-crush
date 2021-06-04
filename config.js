d86c9d35513932ed9971c04174599325eb9cf55b
Lamngoctrai <85330501+Lamngoctrai@users.noreply.github.com>
2021-06-04 12:52:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,15 +4,15 @@ const CONFIG = {
     Làm sao để được nhõng nhẽo với em đêyy
     Ối xời ơi nó mượt
     Cứ thế này anh đổ mất thôi :<`,
-    btnIntro: 'hihi',
-    title: 'Cũng không gì qun trọng 😙',
+    btnIntro: 'hihi :V ',
+    title: 'Cũng không gì quan trọng 😙',
     desc: 'Em ăn cơm chưa??',
-    btnYes: 'Ăn anh <33',
+    btnYes: 'Chưa, Ăn anh <33',
     btnNo: 'Chưa, Ai rảnh :3',
     question:'Iu hok <3',
-    btnReply: 'Gửi cho anh <3',
+    btnReply: 'Gửi cho nè <3',
     reply: 'Iu <33333333',
-    mess: ' moah😘😘',
+    mess: ' moaa ',
     messDesc: 'Tối nay game nhé.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
